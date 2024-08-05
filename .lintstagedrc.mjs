b428f51db5b73a8996ca34677cfc496a7a03c182
조성원 <tjddnjsjo@kakao.com>
2024-08-05 13:47:54 +0900

    chore: configure husky

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,12 @@
+import { relative } from 'node:path'
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => relative(process.cwd(), f)).join(' --file ')}`
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames.map((f) => relative(process.cwd(), f)).join(' ')}`
+
+export default {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx,md,html,css}': [buildPrettierCommand],
+}
