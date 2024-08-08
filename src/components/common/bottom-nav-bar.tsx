7d9d1ecc7849b301f6ac5f50a288edcab2bc2362
조성원 <tjddnjsjo@kakao.com>
2024-08-08 13:18:28 +0900

    feat: bottom-nav-bar 상단 그림자 추가

--- src/components/common/bottom-nav-bar.tsx
+++ src/components/common/bottom-nav-bar.tsx
@@ -15,7 +15,7 @@ export default function BottomNavBar() {
   const pathname = usePathname()
 
   return (
-    <div className="fixed bottom-0 grid w-full max-w-[375px] grid-cols-3 bg-background text-xs">
+    <div className="fixed bottom-0 grid w-full max-w-[375px] grid-cols-3 bg-background text-xs shadow-[0_-2px_5px_0_rgba(0,0,0,0.12)]">
       {links.map((link) => (
         <Link key={`bnb-link-${link.name}`} href={link.path}>
           <div
