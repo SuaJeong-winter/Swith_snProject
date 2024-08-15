5dffad778f425f03db537bffb87e5ef6690930ec
SuaJeong-winter <sua.jeong.developer@gmail.com>
2024-08-15 20:56:02 +0900

    feat: 승인 요청 페이지 퍼블리싱

--- app/waiting/page.tsx
+++ app/waiting/page.tsx
@@ -1,15 +1,73 @@
+import Link from 'next/link'
 import BtnBackIcon from '~/assets/btn_back.svg'
 import BtnMoreVertical from '~/assets/icon_more-vertical.svg'
+import MpProfile from '~/assets/mp_profile.svg'
+import { Button } from '~/components/ui/button'
+import { Chip } from '~/components/ui/chip'
 
 export default function WaitingListPage() {
   return (
     <section className="flex min-h-dvh flex-col bg-white pb-8">
       <div className="fixed top-4 flex flex-row space-x-28 px-3 pt-3">
-        <a href="# ">
+        <Link href="approval">
           <BtnBackIcon />
-        </a>
-        <h2 className="invisible font-bold">스터디 지원하기</h2>
+        </Link>
+        <h2 className="font-bold">대기중인 요청</h2>
         <BtnMoreVertical />
+      </div>
+      <div className="mt-[70px] h-1 w-[375px] border-transparent bg-slate-200"></div>
+
+      <div className="mt-6 space-y-2 px-3">
+        <div>2024년 06월 07일</div>
+        <div className="h-[180px] space-y-2 rounded-lg border-2 border-solid border-gray-400 p-2">
+          <div className="mt-[10px] flex h-[70px] flex-row items-center justify-start space-x-2">
+            <MpProfile />
+            <div className="text-base text-black">
+              <p>김서희</p>
+              <p>기획자</p>
+              <p>스터디 8회</p>
+            </div>
+            <div className="h-[30px] space-x-2 pl-[100px]">
+              <Button size="sm" className="rounded-2xl bg-gray-300 text-black">
+                거절
+              </Button>
+              <Button size="sm" className="rounded-2xl">
+                수락
+              </Button>
+            </div>
+          </div>
+          <p>한줄 자기소개 들어갈 부분</p>
+          <div className="grid h-[10px] grid-cols-4 gap-1 text-xs">
+            <Chip>온라인</Chip>
+            <Chip>오프라인</Chip>
+            <Chip>프론트엔드</Chip>
+          </div>
+        </div>
+        <div>2024년 06월 07일</div>
+        <div className="h-[180px] space-y-2 rounded-lg border-2 border-solid border-gray-400 p-2">
+          <div className="mt-[10px] flex h-[70px] flex-row items-center justify-start space-x-2">
+            <MpProfile />
+            <div className="text-base text-black">
+              <p>김서희</p>
+              <p>기획자</p>
+              <p>스터디 8회</p>
+            </div>
+            <div className="h-[30px] space-x-2 pl-[100px]">
+              <Button size="sm" className="rounded-2xl bg-gray-300 text-black">
+                거절
+              </Button>
+              <Button size="sm" className="rounded-2xl">
+                수락
+              </Button>
+            </div>
+          </div>
+          <p>한줄 자기소개 들어갈 부분</p>
+          <div className="grid h-[10px] grid-cols-4 gap-1 text-xs">
+            <Chip>온라인</Chip>
+            <Chip>오프라인</Chip>
+            <Chip>프론트엔드</Chip>
+          </div>
+        </div>
       </div>
     </section>
   )
