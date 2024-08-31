3f0097741b1b873eb5f9673d719ddad9c52ff0a2
chaerim <limcl8632@gmail.com>
2024-08-31 22:00:39 +0900

    feat: 마이페이지 기능 개발(더미 데이터 확인)

--- app/mypage/open-profile/page.tsx
+++ app/mypage/open-profile/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import { Chip } from '~/components/ui/chip'
 import BtnBack from '~/assets/btn_back.svg'
@@ -5,31 +6,33 @@ import OpenProfileimage from '~/assets/icon_openprofile-image.svg'
 import MeetieMaster from '~/assets/badge_meetie-master.svg'
 import MyStudyPeople from '~/assets/icon_studypeople.svg'
 import { Card } from '~/components/ui/card'
+import useUserController from '~/hooks/useUserController'
+export default function OpenProfile() {
+  // const res = await fetch(
+  //   'https://2cda2bdc-f43f-42c4-862d-6bee7ed3fd16.mock.pstmn.io/user/01',
+  //   {
+  //     cache: 'no-cache',
+  //   },
+  // )
+  // const user = await res.json()
+  // console.log(user)
 
-export default async function OpenProfile() {
-  const res = await fetch(
-    'https://2cda2bdc-f43f-42c4-862d-6bee7ed3fd16.mock.pstmn.io/user/01',
-    {
-      cache: 'no-cache',
-    },
-  )
-  const user = await res.json()
-  console.log(user)
+  // const res2 = await fetch(
+  //   'https://bca20514-f73e-4685-b1ad-7ad95c1392b9.mock.pstmn.io/meetiebadge/01',
+  //   {
+  //     cache: 'no-cache',
+  //   },
+  // )
+  // const meetiebadge = await res2.json()
+  // console.log(meetiebadge)
 
-  const res2 = await fetch(
-    'https://bca20514-f73e-4685-b1ad-7ad95c1392b9.mock.pstmn.io/meetiebadge/01',
-    {
-      cache: 'no-cache',
-    },
-  )
-  const meetiebadge = await res2.json()
-  console.log(meetiebadge)
+  const { user } = useUserController()
 
   return (
     <div className="flex min-h-dvh flex-col items-center bg-background">
       <div className="flex w-full flex-col">
         <div className="flex items-center px-4 pt-4">
-          <Link href="mypage" className="flex items-center">
+          <Link href="/mypage" className="flex items-center">
             <BtnBack className="" />
           </Link>
           <div className="flex flex-1 justify-center">
@@ -39,13 +42,13 @@ export default async function OpenProfile() {
 
         <div className="flex flex-col items-center pt-8">
           <OpenProfileimage className="justify-between" />
-          <span className="pt-2 text-lg font-semibold">{user[0].name}</span>
-          <span className="pt-1 text-xs">{user[0]['job_type']}</span>
+          <span className="pt-2 text-lg font-semibold">{user[0]?.name}</span>
+          <span className="pt-1 text-xs">{user[0]?.['job_type']}</span>
         </div>
 
         <div className="flex flex-col px-4 pb-3 pt-10">
           <span className="text-bold pt-1 font-bold">한줄 자기소개</span>
-          <span className="pt-1 text-xs">{user[0].introduce}</span>
+          <span className="pt-1 text-xs">{user[0]?.introduce}</span>
         </div>
 
         <div className="border-y-2"></div>
@@ -56,10 +59,10 @@ export default async function OpenProfile() {
           <Card className="flex h-[185px] w-[150px] flex-col items-center justify-center border border-2 border-purple-200 border-purple-500 bg-[#FDFBFF] p-5 text-center">
             <MeetieMaster />
             <span className="align-center pt-3 text-xs font-semibold text-gray-500">
-              레벨{meetiebadge[0].level}
+              {/* 레벨{meetiebadge[0].level} */}
             </span>
             <span className="text-xs font-semibold">
-              {meetiebadge[0].badgename}
+              {/* {meetiebadge[0].badgename} */}
             </span>
           </Card>
         </div>
@@ -67,7 +70,7 @@ export default async function OpenProfile() {
         <div className="items-left flex flex-col px-4 pb-5 pt-8">
           <span className="pb-3 text-base font-bold">스터디 목적</span>
           <div className="flex flex-row space-x-4">
-            {user[0]['study_purpose'].map((purpose, idx) => (
+            {user[0]?.['study_purpose'].map((purpose, idx) => (
               <Chip
                 variant="muted"
                 key={purpose + idx}
@@ -81,7 +84,7 @@ export default async function OpenProfile() {
         <div className="items-left flex flex-col px-4">
           <span className="pb-3 text-base font-bold">스터디 성향</span>
           <div className="flex flex-row space-x-4">
-            {user[0]['study_style'].map((style, idx) => (
+            {user[0]?.['study_style'].map((style, idx) => (
               <Chip
                 variant="muted"
                 key={style + idx}
@@ -99,7 +102,7 @@ export default async function OpenProfile() {
               variant="muted"
               className="bg-gray cursor-default border border-meetie-gray-20 text-black"
             >
-              {user[0]['study_period']}
+              {user[0]?.['study_period']}
             </Chip>
           </div>
         </div>
@@ -109,7 +112,7 @@ export default async function OpenProfile() {
             # 성실함과 관심사가 보이는 기록이에요
           </span>
         </div>
-        {user[0]['study_list'].map((list, idx) => (
+        {user[0]?.['study_list'].map((list, idx) => (
           <div
             key={list + idx}
             className="mb-3 ml-4 mr-4 rounded-bl-lg rounded-br-lg rounded-tl-none rounded-tr-lg border border-purple-200 p-4 font-semibold"
---
+++ src/apis/user-rls.ts
@@ -0,0 +1,12 @@
+'use client'
+import { createClient } from '~/utils/supabase/client'
+
+export const getProfile = async () => {
+  const supabase = createClient()
+  const result = await supabase
+    .from('User')
+    .select('*')
+    .eq('user_id', '6803b4bf-455c-4675-8d43-84846d2c5944')
+
+  return result.data
+}
